59ab8277abcccdd76346682693df2257795dfc66
nicklchoi <90160543+nicklchoi@users.noreply.github.com>
2021-12-12 11:03:39 -0800

    Delete src directory

--- src/ros_learning/scripts/action_test.py
+++
@@ -1,6 +0,0 @@
-#import rospy
-#import actionlib
-from my_robot_msgs.msg import CountUntilAction
-from my_robot_msgs.msg import CountUntilGoal
-from my_robot_msgs.msg import CountUntilResult
-from my_robot_msgs.msg import CountUntilFeedback
